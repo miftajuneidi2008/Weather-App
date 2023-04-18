3739d5f9bd89add749f7efa78a95b7cde7b92f57
miftajuneidi2008 <miftajuneidi2008@gmail.com>
2023-04-18 12:43:56 +0300

    React Js weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{useState} from 'react';
+import axios from 'axios';
+import {AiOutlineSearch} from 'react-icons/ai'
+import {BsCloudDrizzle} from 'react-icons/bs';
 
 function App() {
+  const[datas,setDatas]= useState({});
+  const [location,setLocation] = useState('');
+  const url =  `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2074ddacf02e5e5ce1cd8d823c9c03f3`;
+
+  const handleSubmit = e=>
+  {
+    e.preventDefault();
+
+    axios.get(url).then(response=>
+    {
+      return response.data;
+    }).then(data=>{
+      setDatas(data)
+      console.log(datas)
+    })  .catch(error => {
+      console.log(error.response.data.error)
+   })
+   setLocation('')
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    <header>
+      <h2> <BsCloudDrizzle /> Weather App</h2>
+    </header>
+
+    <div className='forms'>
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='search' value={location}  onChange={e=>setLocation(e.target.value)} placeholder='Enter Location' required />
+        <button type='submit'><AiOutlineSearch /></button>
+      </form>
+    </div>
+  <div className='temp-container'>
+    <div className='location'>
+      {datas.name}
+    </div>
+    <div className='temp'>
+     {datas.main ? <p>{datas.main.temp}&deg;F</p> :null}
+    </div>
+    <div className='descriptiion'>
+      {datas.weather?<p>{datas.weather[0].main}</p>:null}
+    </div>
+
+
+    <div className='more'>
+      <div className='humidity'>
+
+        {datas.main ? <p> humidity   {datas.main.humidity}%</p> :null}
+      </div>
+
+      <div className='wind-speed'>
+        {datas.wind ? <p>Wind Speed   {datas.wind.speed}MPH</p> :null}
+      </div>
+
+    </div>
+    </div>
     </div>
   );
 }
